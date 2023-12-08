e7ea0111f546720480619d15688bdd5f976c2ee5
Deepak <20bcs102@nith.ac.in>
2023-12-09 00:01:47 +0530

    Functionality: Search Form

--- vtrade-frontend/src/components/common/Header.jsx
+++ vtrade-frontend/src/components/common/Header.jsx
@@ -1,7 +1,7 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-
+import VehicleSearch from "../vehicle/VehicleSearch";
 export const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
 
@@ -16,14 +16,7 @@ export const Header = () => {
             </span>
           </Link>
         </h1>
-        <form className="pl-2 pt-1 w-80 sm:w-1/3 rounded-lg flex items-center justify-around relative">
-          <input
-            className="transition ease-in-out duration-500 w-[75%] sm:w-full p-2 border-transparent border-b-[1px] bg-transparent relative focus:bg-transparent focus:outline-none focus:border-white focus:border-b-[1px] text-[12px] sm:text-sm hover:border-b-[1px] hover:border-slate-500"
-            type="text"
-            placeholder="Search..."
-          />
-          <FaSearch className=" opacity-[0.5] text-[12px] sm:text-sm absolute right-12 sm:right-4" />
-        </form>
+        <VehicleSearch />
         <ul className="text-white flex gap-2 md:gap-4 lg:gap-8 text-[12px] md:text-[13px] lg:text-sm font-semibold pr-4">
           <li className="transition ease-in-out hover:text-slate-400 hover:font-bold duration-500 hidden sm:inline p-1 pt-2  cursor-pointer border-black border-b-[1px] hover:border-b-[1px] hover:border-slate-500">
             <Link to="/">Home</Link>
---
+++ vtrade-frontend/src/components/vehicle/VehicleSearch.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+const VehicleSearch = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchTerm);
+
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+    console.log("submitted");
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTerm_URL = urlParams.get("searchTerm");
+    setSearchTerm(searchTerm_URL);
+  }, [location.search]);
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="pl-2 pt-1 w-80 sm:w-1/3 rounded-lg flex items-center justify-around relative"
+    >
+      <input
+        onChange={(e) => setSearchTerm(e.target.value)}
+        id="search"
+        className="transition ease-in-out duration-500 w-[75%] sm:w-full p-2 border-transparent border-b-[1px] bg-transparent relative focus:bg-transparent focus:outline-none focus:border-white focus:border-b-[1px] text-[12px] sm:text-sm hover:border-b-[1px] hover:border-slate-500"
+        type="text"
+        placeholder="Search..."
+      />
+      <button className="cursor-pointer transition ease-linear duration-300 opacity-[0.5] hover:opacity-100 text-[12px] sm:text-sm absolute right-12 sm:right-4">
+        <FaSearch className="" />
+      </button>
+    </form>
+  );
+};
+
+export default VehicleSearch;
